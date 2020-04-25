f2d597d91ec133eea269227588589fa3e2fd6dd9
Ahmed Zeeshan Pervaiz <azpervaiz87@gmail.com>
2020-04-25 12:10:49 -0700

    Comments added

--- fltk_test.cpp
+++ fltk_test.cpp
@@ -2,7 +2,7 @@
 #include <Fl/Fl_Box.H>
 #include <Fl/Fl_Window.H>
 #include <string>
-
+// This file is used to just check if fltk lib is working or not
 int main(){
     Fl_Window window(200,200,"Window title");
     Fl_Box box(0,0,200,200," Hey, I mean hello world!");
