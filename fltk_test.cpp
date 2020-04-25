2b233ca5b804ea402f2551feebc1ceda991fc280
Ahmed Zeeshan Pervaiz <zeeshan@pop-os.localdomain>
2020-04-25 11:57:20 -0700

    Original header files from PPP2 (website) along with two source-files fltk_test.cpp test1.cpp

---
+++ fltk_test.cpp
@@ -0,0 +1,11 @@
+#include <FL/Fl.H>
+#include <Fl/Fl_Box.H>
+#include <Fl/Fl_Window.H>
+#include <string>
+
+int main(){
+    Fl_Window window(200,200,"Window title");
+    Fl_Box box(0,0,200,200," Hey, I mean hello world!");
+    window.show();
+    return Fl::run();
+}
